6bab9542d42eac799aa48f1476626d7ce52e1bd2
xueqing@xp123 <xueqing.peng@yale.edu>
2025-05-09 01:41:13 -0400

    es-merge

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# federated_learning
+# # federated_learning
 # autotrain tools merge-llm-adapter \
 #     --base-model-path wendy416/llama32-3b-sft_syn_diabetes \
 #     --adapter-path TheFinAI/fl-cleveland-dpo-qlora \
@@ -23,8 +23,16 @@
 
 
 
-# fino1
+# # fino1
+# autotrain tools merge-llm-adapter \
+#     --base-model-path TheFinAI/Fino1-8B_v2 \
+#     --adapter-path TheFinAI/Fino1-8B-dpo-qlora2 \
+#     --push-to-hub
+
+
+
+# es
 autotrain tools merge-llm-adapter \
-    --base-model-path TheFinAI/Fino1-8B_v2 \
-    --adapter-path TheFinAI/Fino1-8B-dpo-qlora2 \
+    --base-model-path meta-llama/Llama-2-7b-hf \
+    --adapter-path TheFinAI/FinMA-ES-Bilingual \
     --push-to-hub
